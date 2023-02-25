e6d023e2c6049f115f6bdb853e410def319d87cb
Matthew.Grant <Matthew.Grant@ovivowater.com>
2023-02-25 22:22:50 +0100

    Added pickle file and updated the actions, added cors and set up a basic UI

--- server/app.py
+++ server/app.py
@@ -1,5 +1,9 @@
-from flask import Flask
+from flask import Flask, request
+import pandas as pd
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 #load up the semantic similarity
 from sentence_transformers import SentenceTransformer, util
 model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
@@ -8,32 +12,33 @@ model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 def hello():
   return "Server Running!"
 
-#get the sentences from the dataframe?
-#or should we just go one at a time?
-#maybe we just pass a row id and get the triples
-#make sure it's rate limited so as not to kill the server
-@app.route("/sentences")
-def get_sentence():
-    data = request.get_json()
-    hwChannels = data["hwChannels"]  
-    notes = []
-    
-    for hwChannel in hwChannels:
-        sensorNotes = getNotesForSensor(hwChannel)
-        notes.extend(sensorNotes)
-    
-    return jsonify(notes)
 
-@app.route("/similarity")
+@app.route("/sentences", methods=['GET'])
 def get_sentence():
-    sentence1 = ""
-    sentence2 = ""
+    args = request.args
+    line_num = int(args['lineNumber'])
+    print(line_num)
+    file_name = 'matched_deepl_translations.pkl'
+    df = pd.read_pickle(file_name)
+    print(df.shape)
+    print(df)
+    row = df.iloc[:, line_num]
+    print(row)
+    return [row.iloc[0], row.iloc[1]]
+
+
+
+@app.route("/similarity", methods=['POST'])
+def check_similarity():
+    data = request.get_json()
+    sentence1 = data["sentence1"]
+    sentence2 = data["sentence2"]
     #Compute embedding for both lists
     embedding_1= model.encode(sentence1, convert_to_tensor=True)
     embedding_2 = model.encode(sentence2, convert_to_tensor=True)
 
     similarity = util.pytorch_cos_sim(embedding_1, embedding_2)
-    return similarity
+    return {"similarity": similarity.item()}
 
 if __name__ == "__main__":
   app.run()
