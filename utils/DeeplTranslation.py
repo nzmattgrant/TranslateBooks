6d29008ec10dc302cda32af4107f6c1c2b41ab87
Matthew Grant <nzmattgrant@gmail.com>
2023-07-30 11:17:57 +0200

    Adding some more books and updating the home css

---
+++ utils/DeeplTranslation.py
@@ -0,0 +1,44 @@
+import pandas
+import requests
+import pickle
+
+url = 'https://api-free.deepl.com/v2/translate'
+auth_key = 'b73a5d37-53f6-44dd-b44d-de6666e35e8d:fx'
+target_lang = 'EN'
+source_lang = 'DE'
+
+data = {
+    'text': '',
+    'target_lang': target_lang,
+    'source_lang': source_lang
+}
+
+headers = {
+    'Authorization': 'DeepL-Auth-Key ' + auth_key,
+}
+
+translated_texts = []
+
+for sentence in file1_sentences:
+  tokens = nlp(sentence)
+  for token in tokens:
+    if token.is_punct or token.text in dict:
+      #print("word already in dict or is puctuation", token.text)
+      continue
+    print("adding new word", token.text)
+    try:
+      data["text"] = token.text
+      response = requests.post(url, data=data, headers=headers)
+
+      if response.ok:
+          json_data = response.json()
+          translated_text = json_data['translations'][0]['text']
+          dict[token.text] = translated_text
+      else:
+          print('Error:', response.status_code, response.reason)
+    except Exception as e:
+      print("exception occured", e)
+      continue
+
+with open("/content/drive/MyDrive/traslation_dict.pkl", "wb") as f:
+    pickle.dump(dict, f)
