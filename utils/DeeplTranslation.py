3e3d2eccfde3ac2c1545aea1736c09f7416bf1b3
Matthew Grant <nzmattgrant@gmail.com>
2023-07-30 13:31:27 +0200

    translation dict util updated

--- utils/DeeplTranslation.py
+++ utils/DeeplTranslation.py
@@ -3,49 +3,59 @@ import pickle
 from Configuration import DEEPL_API_KEY
 from HumanTranslationMatching import process_file
 import spacy
+import os
 import spacy_transformers
 
 nlp = spacy.load('de_dep_news_trf')
 
-file1_sentences = process_file("/content/drive/MyDrive/Metamorphosis German.txt")
+def update_deepl_translation_dict_from_file(filename):
+    translation_dict = {}
+    translation_dict_path = "traslation_dict.pkl"
+    if os.path.isfile(translation_dict_path):
+        with open(translation_dict_path, "rb") as f:
+            translation_dict = pickle.load(f)[0]
 
-url = 'https://api-free.deepl.com/v2/translate'
-auth_key = DEEPL_API_KEY
-target_lang = 'EN'
-source_lang = 'DE'
+    sentences = process_file(filename)
 
-data = {
-    'text': '',
-    'target_lang': target_lang,
-    'source_lang': source_lang
-}
+    url = 'https://api-free.deepl.com/v2/translate'
+    auth_key = DEEPL_API_KEY
+    target_lang = 'EN'
+    source_lang = 'DE'
 
-headers = {
-    'Authorization': 'DeepL-Auth-Key ' + auth_key,
-}
+    data = {
+        'text': '',
+        'target_lang': target_lang,
+        'source_lang': source_lang
+    }
 
-translated_texts = []
+    headers = {
+        'Authorization': 'DeepL-Auth-Key ' + auth_key,
+    }
 
-for sentence in file1_sentences:
-  tokens = nlp(sentence)
-  for token in tokens:
-    if token.is_punct or token.text in dict:
-      #word already in dict or is puctuation
-      continue
-    print("adding new word", token.text)
-    try:
-      data["text"] = token.text
-      response = requests.post(url, data=data, headers=headers)
+    translated_texts = []
 
-      if response.ok:
-          json_data = response.json()
-          translated_text = json_data['translations'][0]['text']
-          dict[token.text] = translated_text
-      else:
-          print('Error:', response.status_code, response.reason)
-    except Exception as e:
-      print("exception occured", e)
-      continue
+    for sentence in sentences:
+        tokens = nlp(sentence)
+        for token in tokens:
+            if token.is_punct or token.text in translation_dict:
+                #word already in dict or is puctuation
+                continue
+            print("adding new word", token.text)
+            try:
+                data["text"] = token.text
+                response = requests.post(url, data=data, headers=headers)
 
-with open("/content/drive/MyDrive/traslation_dict.pkl", "wb") as f:
-    pickle.dump(dict, f)
+                if response.ok:
+                    json_data = response.json()
+                    translated_text = json_data['translations'][0]['text']
+                    dict[token.text] = translated_text
+                else:
+                    print('Error:', response.status_code, response.reason)
+            except Exception as e:
+                print("exception occured", e)
+                continue
+
+    with open("traslation_dict.pkl", "wb") as f:
+        pickle.dump(translation_dict, f)
+
+update_deepl_translation_dict_from_file("Hänsel und Gretel.txt")
